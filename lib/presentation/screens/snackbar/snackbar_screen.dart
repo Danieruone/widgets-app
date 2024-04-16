79e4376d75e26ed89af297d4dd0c9e4ec455597b
Daniel Mendoza <danieruone@gmail.com>
2024-04-15 20:15:04 -0500

    :sparkles: Add floating action button

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -5,6 +5,15 @@ class SnackBarScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Snackbars and dialogs"),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        label: const Text("Show Snackbar"),
+        onPressed: () {},
+        icon: const Icon(Icons.remove_red_eye_outlined),
+      ),
+    );
   }
 }
