a421cce237c63973bb09e62a5b9e0cb3d468e3ef
Daniel Mendoza <danieruone@gmail.com>
2024-04-17 20:39:47 -0500

    :sparkles: Add animated container screen

---
+++ lib/presentation/screens/animated/animated_container_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AnimatedContainerScreen extends StatelessWidget {
+  const AnimatedContainerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Animated container"),
+      ),
+    );
+  }
+}
